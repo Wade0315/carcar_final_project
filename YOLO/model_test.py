679a3c35d692fc923a841007b514d80e1b7f5406
waryt <warytf5133@gmail.com>
2026-06-16 14:10:47 +0800

    add does not find head

--- YOLO/model_test.py
+++ YOLO/model_test.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO(r"/home/waryt/carcar_final_project/runs/detect/shuttle_yolov5nu_256_v2/weights/best.pt")
+model = YOLO(r"/home/waryt/carcar_final_project/runs/detect/shuttle_yolov5nu_256_v3/weights/best.pt")
 results = model.predict(
-    source=r"/home/waryt/carcar_final_project/YOLO/shuttle.v5i.yolov5pytorch/valid/images",
+    source=r"/home/waryt/carcar_final_project/YOLO/shuttle.v6i.yolov5pytorch/test/images",
     imgsz=256,
     conf=0.25,
     save=True,
-    project=r"/home/waryt/carcar_final_project/runs/detect_v5nu_v2",
+    project=r"/home/waryt/carcar_final_project/runs/detect_v5nu_v3",
     name="predict"
 
 )
